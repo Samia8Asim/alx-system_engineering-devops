c364c1873b69ecf26c329b67c9fd848e760bb701
Samia8Asim <samia2003.8@gmail.com>
2024-01-30 07:08:30 +0200

    1

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,12 +1,2 @@
 #!/usr/bin/env ruby
-
-regx = /School/
-
-in_str = ARGV[0]
-match = in_str.scan(regx)
-
-if match.any?
-  puts "#{match.join}"
-else
-  puts ""
-end
+puts ARGV[0].scan(/School/).join
--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,12 +1,2 @@
 #!/usr/bin/env ruby
-
-regx = /hbt{2,5}n/
-
-in_str = ARGV[0]
-match = in_str.scan(regx)
-
-if match.any?
-  puts "#{match.join}"
-else
-  puts ""
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
